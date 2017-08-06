b7e343a1fccf7450b5246055974aee452c294d9d
Dino Pelic <nodi@macs-MacBook-Air.local>
2017-08-06 11:59:51 +0200

    Splash screen with fade-in logo

--- WashVes Native/ViewController.swift
+++ WashVes Native/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var splashImage: UIImageView!
+    var timer: Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        splashImage.transform = CGAffineTransform.init(scaleX: 0.05, y: 0.05)
+        
+        UIView.animate(withDuration: 2.0, delay: 0, options: .curveEaseIn, animations: {
+         self.splashImage.transform = CGAffineTransform.identity
+        }, completion: {
+            success in
+            if success {
+                self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.showGetStartedScreen), userInfo: nil, repeats: false)
+            }
+        })
+    }
+    
+    func showGetStartedScreen () {
+        let sb = UIStoryboard(name: "GetStarted", bundle: nil)
+        let getStartedVC = sb.instantiateViewController(withIdentifier: "getStarted")
+        self.present(getStartedVC, animated: false, completion: nil)
+
     }
 
     override func didReceiveMemoryWarning() {
